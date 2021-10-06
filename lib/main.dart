2ea3c16212dd5c5eed8a7528487c639af22b5a7d
Nahin-CDR <dev.nahin95@gmail.com>
2021-10-06 19:24:48 +0600

    first push

---
+++ lib/main.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+//Coder : Nahin the Coder
+//Date  : 03-10-2021
+
+
+main()
+{
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      home: Scaffold(
+        // appBar: AppBar(
+        //   title: Text(
+        //     "Hello App",
+        //     style: TextStyle(
+        //       fontSize: 18,
+        //     ),
+        //   ),
+        // ),
+      ),
+    );
+
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// void main()
+// {
+//   runApp(MaterialApp(
+//     home: Scaffold(
+//       appBar: AppBar(
+//         title: Text(
+//             "My App",
+//             style: TextStyle(),
+//         ),
+//         backgroundColor: Colors.red[600],
+//       ),
+//       body: Center(
+//         child: Text(
+//           "this is just begining",
+//               style: TextStyle(
+//             //backgroundColor: Colors.red,
+//             color: Colors.red, fontSize: 20,
+//                 fontWeight: FontWeight.bold,
+//         ),
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: () {
+//         },
+//         child: Text('click'),
+//         backgroundColor: Colors.red,
+//       ),
+//     ),
+//   ));
+// }
+//
+//
+//
